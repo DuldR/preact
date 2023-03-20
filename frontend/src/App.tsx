fbe752ca0788b5020f00c0fdde6ecc1d9d46de57
Garrett <gnjulaka@gmail.com>
2023-03-20 15:53:34 -0500

    Routes setup for a home page and a settings page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom"
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      window.location.replace("/app")
+    }
+  }, [])
 
   return (
-    <h1>
-      What's rool good
-    </h1>
+    <BrowserRouter basename="app">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
+    </BrowserRouter>
   )
+}
+
+function SettingsPage() {
+
+  return (
+    <div>
+      <h1> Settings Page </h1>
+    </div>
+  )
+}
+
+function HomePage() {
+  const style = { padding : "8px" }
+
+  return (
+    <div style={style} >
+      <h1> Home Page Yall </h1>
+    </div>
+
+  )
+
+
 }
 
 export default App
